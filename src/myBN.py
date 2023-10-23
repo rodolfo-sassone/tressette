7876940b29caf53a918ba09745c5c879291efa2a
rodolfo-sassone <r.sassone3@studenti.uniba.it>
2023-10-23 16:40:22 +0200

    Aggiunti file per BN

---
+++ src/myBN.py
@@ -0,0 +1,219 @@
+from aipython.probVariables import Variable
+from aipython.probFactors import Prob
+from aipython.probGraphicalModels import BeliefNetwork
+from aipython.probVE import VE 
+from enum import Enum
+import numpy as np
+
+boolean = [True, False]
+
+class Palo(Enum):
+    DENARI = 0
+    COPPE = 1
+    SPADE = 2
+    BASTONI = 3
+
+palo = Variable('Palo',[Palo.DENARI.value, Palo.COPPE.value, Palo.SPADE.value, Palo.BASTONI.value])
+quattro_denari = Variable('quattro denari', boolean)
+cinque_denari = Variable('cinque denari', boolean)
+sei_denari = Variable('sei denari', boolean)
+sette_denari = Variable('sette denari', boolean)
+otto_denari = Variable('otto denari', boolean)
+nove_denari = Variable('nove denari', boolean)
+dieci_denari = Variable('dieci denari', boolean)
+asso_denari = Variable('asso denari', boolean)
+due_denari = Variable('due denari', boolean)
+tre_denari = Variable('tre denari', boolean)
+quattro_coppe = Variable('quattro coppe', boolean)
+cinque_coppe = Variable('cinque coppe', boolean)
+sei_coppe = Variable('sei coppe', boolean)
+sette_coppe = Variable('sette coppe', boolean)
+otto_coppe = Variable('otto coppe', boolean)
+nove_coppe = Variable('nove coppe', boolean)
+dieci_coppe = Variable('dieci coppe', boolean)
+asso_coppe = Variable('asso coppe', boolean)
+due_coppe = Variable('due coppe', boolean)
+tre_coppe = Variable('tre coppe', boolean)
+quattro_spade = Variable('quattro spade', boolean)
+cinque_spade = Variable('cinque spade', boolean)
+sei_spade = Variable('sei spade', boolean)
+sette_spade = Variable('sette spade', boolean)
+otto_spade = Variable('otto spade', boolean)
+nove_spade = Variable('nove spade', boolean)
+dieci_spade = Variable('dieci spade', boolean)
+asso_spade = Variable('asso spade', boolean)
+due_spade = Variable('due spade', boolean)
+tre_spade = Variable('tre spade', boolean)
+quattro_bastoni = Variable('quattro bastoni', boolean)
+cinque_bastoni = Variable('cinque bastoni', boolean)
+sei_bastoni = Variable('sei bastoni', boolean)
+sette_bastoni = Variable('sette bastoni', boolean)
+otto_bastoni = Variable('otto bastoni', boolean)
+nove_bastoni = Variable('nove bastoni', boolean)
+dieci_bastoni = Variable('dieci bastoni', boolean)
+asso_bastoni = Variable('asso bastoni', boolean)
+due_bastoni = Variable('due bastoni', boolean)
+tre_bastoni = Variable('tre bastoni', boolean)
+
+carta_denari =  Variable('carta denari',[0,1,2,3,4,5,6,7,8,9])
+carta_coppe = Variable('carta coppe',[0,1,2,3,4,5,6,7,8,9])
+carta_spade = Variable('carta spade',[0,1,2,3,4,5,6,7,8,9])
+carta_bastoni = Variable('carta bastoni',[0,1,2,3,4,5,6,7,8,9])
+
+quattro = 0.25
+cinque = 0.15
+sei = 0.12
+sette = 0.1
+otto = 0.09
+nove = 0.09
+dieci = 0.09
+asso = 0.02
+due = 0.07
+tre = 0.02
+
+coeff_palo = 0.85
+coeff_notPalo = 0.05
+def carta_palo(carta):
+    return carta*coeff_palo
+
+def carta_notPalo(carta):
+    return carta*coeff_notPalo
+
+def get_prob(var,pars,palo,value):
+      if palo == Palo.DENARI or palo == 0: 
+            f = Prob(var,pars,[[(1-carta_palo(value)), carta_palo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)]])
+            
+      elif palo == Palo.COPPE or palo == 1:
+           f = Prob(var,pars,[[(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_palo(value)), carta_palo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)]])
+           
+      elif palo == Palo.SPADE or palo == 2:
+           f = Prob(var,pars,[[(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_palo(value)), carta_palo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)]])
+           
+      elif palo == Palo.BASTONI or palo == 3:
+            f = Prob(var,pars,[[(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_notPalo(value)), carta_notPalo(value)],
+                              [(1-carta_palo(value)), carta_palo(value)]])
+      
+      return f
+
+f_palo = Prob(palo,[],[0.25,0.25,0.25,0.25])
+#DENARI
+f_4d = get_prob(quattro_denari,[palo],Palo.DENARI,quattro)
+f_5d = get_prob(cinque_denari,[palo],Palo.DENARI,cinque)
+f_6d = get_prob(sei_denari,[palo],Palo.DENARI,sei)
+f_7d = get_prob(sette_denari,[palo],Palo.DENARI,sette)
+f_8d = get_prob(otto_denari,[palo],Palo.DENARI,otto)
+f_9d = get_prob(nove_denari,[palo],Palo.DENARI,nove)
+f_10d = get_prob(dieci_denari,[palo],Palo.DENARI,dieci)
+f_1d = get_prob(asso_denari,[palo],Palo.DENARI,asso)
+f_2d = get_prob(due_denari,[palo],Palo.DENARI,due)
+f_3d = get_prob(tre_denari,[palo],Palo.DENARI,tre)
+#COPPE
+f_4c = get_prob(quattro_coppe,[palo],Palo.COPPE,quattro)
+f_5c = get_prob(cinque_coppe,[palo],Palo.COPPE,cinque)
+f_6c = get_prob(sei_coppe,[palo],Palo.COPPE,sei)
+f_7c = get_prob(sette_coppe,[palo],Palo.COPPE,sette)
+f_8c = get_prob(otto_coppe,[palo],Palo.COPPE,otto)
+f_9c = get_prob(nove_coppe,[palo],Palo.COPPE,nove)
+f_10c = get_prob(dieci_coppe,[palo],Palo.COPPE,dieci)
+f_1c = get_prob(asso_coppe,[palo],Palo.COPPE,asso)
+f_2c = get_prob(due_coppe,[palo],Palo.COPPE,due)
+f_3c = get_prob(tre_coppe,[palo],Palo.COPPE,tre)
+#SPADE
+f_4s = get_prob(quattro_spade,[palo],Palo.SPADE,quattro)
+f_5s = get_prob(cinque_spade,[palo],Palo.SPADE,cinque)
+f_6s = get_prob(sei_spade,[palo],Palo.SPADE,sei)
+f_7s = get_prob(sette_spade,[palo],Palo.SPADE,sette)
+f_8s = get_prob(otto_spade,[palo],Palo.SPADE,otto)
+f_9s = get_prob(nove_spade,[palo],Palo.SPADE,nove)
+f_10s = get_prob(dieci_spade,[palo],Palo.SPADE,dieci)
+f_1s = get_prob(asso_spade,[palo],Palo.SPADE,asso)
+f_2s = get_prob(due_spade,[palo],Palo.SPADE,due)
+f_3s = get_prob(tre_spade,[palo],Palo.SPADE,tre)
+#BASTONI
+f_4b = get_prob(quattro_bastoni,[palo],Palo.BASTONI,quattro)
+f_5b = get_prob(cinque_bastoni,[palo],Palo.BASTONI,cinque)
+f_6b = get_prob(sei_bastoni,[palo],Palo.BASTONI,sei)
+f_7b = get_prob(sette_bastoni,[palo],Palo.BASTONI,sette)
+f_8b = get_prob(otto_bastoni,[palo],Palo.BASTONI,otto)
+f_9b = get_prob(nove_bastoni,[palo],Palo.BASTONI,nove)
+f_10b = get_prob(dieci_bastoni,[palo],Palo.BASTONI,dieci)
+f_1b = get_prob(asso_bastoni,[palo],Palo.BASTONI,asso)
+f_2b = get_prob(due_bastoni,[palo],Palo.BASTONI,due)
+f_3b = get_prob(tre_bastoni,[palo],Palo.BASTONI,tre)
+
+def get_row(array, i, size):
+    if size == 1:
+        value = []
+        for b in reversed(array):
+            if b:
+                c = np.count_nonzero(array == True)
+                value.append(float(1/c))
+            else:
+                value.append(float(0))
+
+        return value
+    
+    elif size%2 == 0:
+        i = i+1
+
+        array2 = np.copy(array)
+        array2[i] = True
+
+        row1 = get_row(array, i, size/2)
+        row2 = get_row(array2, i, size/2)
+
+    return [row1,row2]
+
+
+def get_TabularCPD(var):
+    array1 = np.zeros(var.get_size(), dtype=bool)
+    array2 = np.zeros(var.get_size(), dtype=bool)
+    i = 0
+    array2[i] = True
+
+    row1 = get_row(array1, i, (2**var.get_size())/2)
+    row2 = get_row(array2, i, (2**var.get_size())/2)
+
+    return [row1,row2]
+
+f_cd = Prob(carta_denari, [tre_denari,due_denari,asso_denari,dieci_denari,nove_denari,otto_denari,sette_denari,sei_denari,cinque_denari,quattro_denari], get_TabularCPD(carta_denari))
+f_cc = Prob(carta_coppe, [tre_coppe,due_coppe,asso_coppe,dieci_coppe,nove_coppe,otto_coppe,sette_coppe,sei_coppe,cinque_coppe,quattro_coppe], get_TabularCPD(carta_coppe))
+f_cs =  Prob(carta_spade, [tre_spade,due_spade,asso_spade,dieci_spade,nove_spade,otto_spade,sette_spade,sei_spade,cinque_spade,quattro_spade],get_TabularCPD(carta_spade))
+f_cb = Prob(carta_bastoni, [tre_bastoni,due_bastoni,asso_bastoni,dieci_bastoni,nove_bastoni,otto_bastoni,sette_bastoni,sei_bastoni,cinque_bastoni,quattro_bastoni],get_TabularCPD(carta_bastoni))
+
+tressette = BeliefNetwork("Probabilita' prossima carta", {palo,
+                                                          quattro_denari,cinque_denari,sei_denari,sette_denari,otto_denari,nove_denari,dieci_denari,asso_denari,due_denari,tre_denari,
+                                                          quattro_coppe,cinque_coppe,sei_coppe,sette_coppe,otto_coppe,nove_coppe,dieci_coppe,asso_coppe,due_coppe,tre_coppe,
+                                                          quattro_spade,cinque_spade,sei_spade,sette_spade,otto_spade,nove_spade,dieci_spade,asso_spade,due_spade,tre_spade,
+                                                          quattro_bastoni,cinque_bastoni,sei_bastoni,sette_bastoni,otto_bastoni,nove_bastoni,dieci_bastoni,asso_bastoni,due_bastoni,tre_bastoni,
+                                                          carta_denari,carta_coppe,carta_bastoni,carta_spade},
+                                                          {f_palo,
+                                                           f_4d,f_5d,f_6d,f_7d,f_8d,f_9d,f_10d,f_1d,f_2d,f_3d,
+                                                           f_4c,f_5c,f_6c,f_7c,f_8c,f_9c,f_10c,f_1c,f_2c,f_3c,
+                                                           f_4s,f_5s,f_6s,f_7s,f_8s,f_9s,f_10s,f_1s,f_2s,f_3s,
+                                                           f_4b,f_5b,f_6b,f_7b,f_8b,f_9b,f_10b,f_1b,f_2b,f_3b,
+                                                           f_cd, f_cc, f_cs, f_cb})
+
+class TressetteVE(VE):
+    def __init__(self, gm=tressette):
+        super().__init__(gm)
+
+    def query(self,var,obs={},elim_order=None):
+        super().query(self,var,obs,elim_order)
+
+    def project_observations(self,factor,obs):
+        super().project_observations(self,factor,obs)
+
+    def eliminate_var(self,factors,var):
+        super().eliminate_var(self,factors,var)
